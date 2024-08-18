27d02ea175acaa24548f816fce9c26cd388abc99
harryzhu <harry.chuh@gmail.com>
2024-08-18 12:36:39 +0800

    -

--- api.webui.plus/app/controllers/BaseController.php
+++ api.webui.plus/app/controllers/BaseController.php
@@ -2,12 +2,20 @@
 require_once(BASE_PATH.'/app/library/vendor/autoload.php');
 use Phalcon\Mvc\Dispatcher;
 use Phalcon\Mvc\Controller;
+use Phalcon\Mvc\View;
 use Phalcon\Http\Response;
 
 class BaseController extends Controller
 {
 	public $data;
 	public function initialize(){
+		$this->view->setRenderLevel(
+            View::LEVEL_NO_RENDER
+        );
+        $this->response->setStatusCode(200, 'OK');
+        $this->response->setContentType('application/json', 'UTF-8');
+        $this->response->setHeader('Cache-Control', 'no-store');
+        
 		$this->data = array();
 		
 	}
@@ -15,9 +23,7 @@ class BaseController extends Controller
 
 public function jsonOut()
     {
-        $this->view->disable();
-        $this->response->setContentType('application/json', 'UTF-8');
-        $this->response->setHeader('Cache-Control', 'no-store');
+        
 
 
         $this->response->setJsonContent($this->data);
