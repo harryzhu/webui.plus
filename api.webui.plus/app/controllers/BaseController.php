97ead66d23e421da1c7f4db48503ff686672a3c8
harryzhu <harry.chuh@gmail.com>
2024-08-18 12:07:29 +0800

    -

--- api.webui.plus/app/controllers/BaseController.php
+++ api.webui.plus/app/controllers/BaseController.php
@@ -6,24 +6,27 @@ use Phalcon\Http\Response;
 
 class BaseController extends Controller
 {
-	public $data;
-
 	public function initialize(){
-		$this->data = array();
+		
 		
 	}
 
 
-	public function afterExecuteRoute($dispatcher){
-		$this->view->disable();
-		$this->response->setContentType('application/json', 'UTF-8');
-		$this->response->setHeader('Cache-Control', 'no-store');
+public function afterExecuteRoute($dispatcher)
+    {
+        $this->view->disable();
+        $this->response->setContentType('application/json', 'UTF-8');
+        $this->response->setHeader('Cache-Control', 'no-store');
 
-		if (true !== $this->response->isSent()) {
-			$this->response->setJsonContent($this->data);
+        /** @var array $data */
+        $data = $dispatcher->getReturnedValue();
+        $dispatcher->setReturnedValue([]);
 
-			return $this->response->send();
-		}
-	}
+        if (true !== $this->response->isSent()) {
+            $this->response->setJsonContent($data);
+
+            return $this->response->send();
+        }
+    }
 
 }
